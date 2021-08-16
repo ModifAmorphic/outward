65e419393b5dfcd7958524bae99aba72eff23474
Modif Amorphic <modifamorphic@gmail.com>
2021-08-15 18:35:22 -0700

    Removed Enums. Moved back to stashpacks. Caused issue "Could not resolve type with token"

--- Outward.Shared/ModifAmorphic.Outward.Shared/Extensions/EnumExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace ModifAmorphic.Outward.Extensions
-{
-    public static class EnumExtensions
-    {
-        public static string GetName<T>(this T enumValue) where T : Enum => Enum.GetName(typeof(T), enumValue);
-    }
-}
