54606ca008aed903a99bcd76f9bdef387319c01e
Modif Amorphic <modifamorphic@gmail.com>
2021-08-16 14:32:41 -0700

    PR fixes. Removed commented code

--- StashPacks/StashPacks/Patch/Events/NetworkLevelLoaderEvents.cs
+++ StashPacks/StashPacks/Patch/Events/NetworkLevelLoaderEvents.cs
@@ -8,23 +8,7 @@ namespace ModifAmorphic.Outward.StashPacks.Patch.Events
         private static IModifLogger Logger => LoggerFactory?.Invoke();
         public static Func<IModifLogger> LoggerFactory { get; set; }
 
-        //public static event Action<OnJoinedRoomArgs> OnJoinedRoomBefore;
         public static event Action<NetworkLevelLoader> MidLoadLevelBefore;
-
-        //public static void RaiseOnJoinedRoomBefore(NetworkLevelLoader networkLevelLoader, bool failedJoin)
-        //{
-        //    try
-        //    {
-        //        Logger?.LogTrace($"{nameof(NetworkLevelLoaderEvents)}::{nameof(RaiseOnJoinedRoomBefore)} triggered.");
-        //        OnJoinedRoomBefore?.Invoke(new OnJoinedRoomArgs(networkLevelLoader, failedJoin));
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Logger?.LogException($"Exception in {nameof(NetworkLevelLoaderEvents)}::{nameof(RaiseOnJoinedRoomBefore)}.", ex);
-        //        if (Logger == null)
-        //            UnityEngine.Debug.LogError($"Exception in {nameof(NetworkLevelLoaderEvents)}::{nameof(RaiseOnJoinedRoomBefore)}:\n{ex}");
-        //    }
-        //}
 
         public static void RaiseMidLoadLevelBefore(NetworkLevelLoader networkLevelLoader)
         {
--- StashPacks/StashPacks/Patch/NetworkLevelLoaderPatches.cs
+++ StashPacks/StashPacks/Patch/NetworkLevelLoaderPatches.cs
@@ -6,12 +6,6 @@ namespace ModifAmorphic.Outward.StashPacks.Patch
     [HarmonyPatch(typeof(NetworkLevelLoader))]
     internal static class NetworkLevelLoaderPatches
     {
-        //[HarmonyPatch(nameof(NetworkLevelLoader.OnJoinedRoom), MethodType.Normal)]
-        //[HarmonyPrefix]
-        //private static void OnJoinedRoomPrefix(NetworkLevelLoader __instance, bool ___m_failedJoin)
-        //{
-        //    NetworkLevelLoaderEvents.RaiseOnJoinedRoomBefore(__instance, ___m_failedJoin);
-        //}
 
         [HarmonyPatch(nameof(NetworkLevelLoader.MidLoadLevel), MethodType.Normal)]
         [HarmonyPrefix]
